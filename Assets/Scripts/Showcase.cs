e0e63c4b1039b6e73b0f290593f8c9d4e7cafc7d
BradleySeymourSAE <1023303@student.sae.edu.au>
2021-06-18 13:36:30 +1000

    Added Environmental Effects & Fixed Snowfall error

--- Assets/Scripts/Showcase.cs
+++ Assets/Scripts/Showcase.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using System.Linq;
 #endregion
 
 
@@ -14,22 +15,9 @@ public class Showcase : MonoBehaviour
 
 	public enum SnowStates { Active, InActive };
 
-	public static Showcase instance;
-	public static UnityEvent ToggleSnowEvent = new UnityEvent();
-
-	public ParticleSystem SnowParticleSystem;
-
-	[Range(0.01f, 0.1f)] public float snowSpeed;
-
-	#region Private Variables
-
-	[SerializeField] private float m_SnowLevelValue = 0;
-	[SerializeField] private bool m_SnowfallEnabled = false;
-
-
 	public SnowStates currentSnowState
-	{ 
-		get 
+	{
+		get
 		{
 			if (m_SnowfallEnabled)
 				return SnowStates.Active;
@@ -38,12 +26,54 @@ public class Showcase : MonoBehaviour
 		}
 	}
 
+	public static Showcase instance;
+	public static UnityEvent ToggleSnowEvent = new UnityEvent();
+	public static UnityEvent ToggleEnvironmentChange = new UnityEvent();
+	public GameObject snowSystemPrefab;
+	public GameObject firePrefab;
 
-#endregion
 
-#region Unity References
+	[Range(0f, 1f)]
+	public float elementScaleX = 0.75f;
+	[Range(0f, 1f)]
+	public float elementScaleY = 0.5f;
 
-private void OnEnable()
+	[Range(-5f, 5f)]
+	public float elementOffsetYPosition = 3f;
+	
+	[Range(0.01f, 0.1f)]
+	public float snowSpeed = 0.05f;
+
+	[SerializeField] 
+	protected ParticleSystem[] m_SnowParticleSystems;
+	[SerializeField] protected Terrain m_Terrain;
+	[SerializeField] protected Light m_DirectionalLight;
+	[SerializeField] protected Color m_EnvironmentColor;
+
+
+	[SerializeField]
+	protected Vector3 m_EnvironmentalOffsetPosition = Vector3.zero;
+
+	[SerializeField] 
+	private float m_SnowLevelValue = 0;
+	
+	[TagSelector] 
+	[SerializeField] 
+	protected string m_TagSelector;
+
+	[SerializeField] 
+	protected List<Transform> m_EnvironmentSpawnPositions = new List<Transform>();
+	
+	[SerializeField]
+	protected List<GameObject> m_SpawnedEffects = new List<GameObject>();
+
+	private bool m_SnowfallEnabled;
+	private bool m_EnvironmentChanged;
+
+
+	#region Unity References
+
+	private void OnEnable()
 	{
 		if (instance != null && instance != this)
 			Destroy(instance.gameObject);
@@ -51,40 +81,115 @@ private void OnEnable()
 		instance = this;
 
 		m_SnowfallEnabled = false;
-		m_SnowLevelValue = 0;
+		m_EnvironmentChanged = false;
+		m_EnvironmentalOffsetPosition.y -= elementOffsetYPosition;
 		
-		if (SnowParticleSystem.isPlaying)
-		{
-			SnowParticleSystem.Stop();
+		GameObject s_SnowfallPrefab = Instantiate(snowSystemPrefab, new Vector3(0, 40, 0), Quaternion.identity);
+
+		for (var i = 0; i < s_SnowfallPrefab.GetComponentsInChildren<ParticleSystem>().Length; i++)
+		{ 
+			m_SnowParticleSystems = s_SnowfallPrefab.GetComponentsInChildren<ParticleSystem>();
+
+			ParticleSystem s_ParticleSystem = m_SnowParticleSystems[i];
+
+			if (s_ParticleSystem.isPlaying)
+				s_ParticleSystem.Stop();
 		}
 
-
 		ToggleSnowEvent.AddListener(ToggleSnow);
+		ToggleEnvironmentChange.AddListener(() =>
+		{
+			SetEnvironment();
+		});
 	}
 
 	private void OnDisable()
 	{
 		ToggleSnowEvent.RemoveListener(ToggleSnow);
+		ToggleEnvironmentChange.RemoveListener(() =>
+		{
+			SetEnvironment();
+		});
+	}
+
+	private void Start()
+	{
+		foreach (var s_SpawnPosition in GameObject.FindGameObjectsWithTag(m_TagSelector))
+		{
+			m_EnvironmentSpawnPositions.Add(s_SpawnPosition.transform);
+		}
+		m_EnvironmentChanged = false;
 	}
 
 	private void Update()
 	{
+		UpdateSnowfallLevel();
+		
+	}
+
+	private void UpdateSnowfallLevel()
+	{ 
 		if (!m_SnowfallEnabled)
 		{
-			return;
+			if (m_SnowLevelValue <= 0) 
+			{ 
+				m_SnowLevelValue = 0;
+			}
+			else if (m_SnowLevelValue > 0 && m_SnowLevelValue <= 1f)
+			{
+				m_SnowLevelValue -= Time.deltaTime * snowSpeed;
+			}
 		}
 		
 		if (m_SnowfallEnabled)
-		{ 
-
-			if (m_SnowLevelValue < 1f)
+		{
+			if (m_SnowLevelValue <= 1f)
 			{
-				Shader.SetGlobalFloat("_SnowLevel", m_SnowLevelValue);
 				m_SnowLevelValue += Time.deltaTime * snowSpeed;
 			}
 		}
+
+		Shader.SetGlobalFloat("_SnowLevel", m_SnowLevelValue);
 	}
 
+
+	private void SetEnvironment()
+	{
+
+		// If the environment has already been changed (Fire has been spawned in) 
+		if (m_EnvironmentChanged && m_SpawnedEffects.Count > 0)
+		{
+			// Set environment to not be changed anymore 
+			m_EnvironmentChanged = false;
+
+			foreach (GameObject effect in m_SpawnedEffects) { Destroy(effect); }
+
+			m_DirectionalLight.color = Color.white;
+		}
+		else if (!m_EnvironmentChanged)
+		{
+			m_EnvironmentChanged = true;
+
+			if (m_EnvironmentSpawnPositions.Count > 0)
+			{
+				m_SpawnedEffects.Clear();
+
+				for (int i = 0; i < m_EnvironmentSpawnPositions.Count; i++)
+				{ 
+					Vector3 s_SpawnPosition = m_EnvironmentSpawnPositions[Random.Range(1, m_EnvironmentSpawnPositions.Count / 2)].position;
+
+					s_SpawnPosition += m_EnvironmentalOffsetPosition;
+
+					GameObject s_SpawnedFire = Instantiate(firePrefab, s_SpawnPosition, Quaternion.identity);
+
+					s_SpawnedFire.transform.localScale = new Vector3(elementScaleX, elementScaleY, elementScaleX);
+					m_SpawnedEffects.Add(s_SpawnedFire);
+				}
+
+				m_DirectionalLight.color = m_EnvironmentColor;
+			}
+		}
+	}
 
 
 	/// <summary>
@@ -93,18 +198,29 @@ private void OnEnable()
 	/// <param name="ShouldEnableSnowfall"></param>
 	private void ToggleSnow()
 	{
-		Debug.Log("Toggling Snow Enabled: " + m_SnowfallEnabled);
 
 		if (currentSnowState.Equals(SnowStates.Active))
 		{
 			m_SnowfallEnabled = false;
+
+			foreach (ParticleSystem s_SnowEffect in m_SnowParticleSystems)
+			{
+				if (s_SnowEffect.isPlaying)
+					s_SnowEffect.Stop();
+			}
 		}
 		else if (currentSnowState.Equals(SnowStates.InActive))
 		{
 			m_SnowfallEnabled = true;
+
+			foreach (ParticleSystem s_SnowEffect in m_SnowParticleSystems)
+			if (!s_SnowEffect.isPlaying)
+			{
+				s_SnowEffect.Play();
+			}
 		}
 
-		Debug.Log("Snowfall Enabled? " + m_SnowfallEnabled);
+		Debug.Log("Toggling Snow Enabled: " + m_SnowfallEnabled);
 	}
 
 	#endregion
--- Assets/Shaders/SnowShader.shader
+++ Assets/Shaders/SnowShader.shader
@@ -36,24 +36,12 @@ Shader "Custom/SnowShader"
         fixed4 _SnowColor;
         float _SnowLevel;
 
-        // Add instancing support for this shader. You need to check 'Enable Instancing' on materials that use the shader.
-        // See https://docs.unity3d.com/Manual/GPUInstancing.html for more information about instancing.
-        // #pragma instancing_options assumeuniformscaling
-        UNITY_INSTANCING_BUFFER_START(Props)
-            // put more per-instance properties here
-        UNITY_INSTANCING_BUFFER_END(Props)
 
         void surf (Input IN, inout SurfaceOutputStandard o)
         {
-
             fixed4 s = (_SnowColor * IN.worldNormal.y) * _SnowLevel;
-
-
-            // Albedo comes from a texture tinted by color
             fixed4 c = tex2D (_MainTex, IN.uv_MainTex) * _Color;
             o.Albedo = c.rgb + s.rgb;
-
-            // Metallic and smoothness come from slider variables
             o.Metallic = _Metallic;
             o.Smoothness = _Glossiness;
             o.Alpha = c.a;
